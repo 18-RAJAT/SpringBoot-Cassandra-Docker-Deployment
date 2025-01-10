c47ed64b21e255197f5c6785be077f223f853dbf
18-RAJAT <Rajatjoshi7535@gmail.com>
2025-01-10 12:38:57 +0530

    Project Init

---
+++ src/main/java/springboot_cassandra_integration/springboot_cassandra_integration/SpringbootCassandraIntegrationApplication.java
@@ -0,0 +1,13 @@
+package springboot_cassandra_integration.springboot_cassandra_integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCassandraIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCassandraIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/springboot_cassandra_integration/springboot_cassandra_integration/SpringbootCassandraIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package springboot_cassandra_integration.springboot_cassandra_integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCassandraIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
